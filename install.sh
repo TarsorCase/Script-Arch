cd8a344a1895faf6b9080e67f4f0248cedf9ecb5
TarsorCase <gerardgonzalez41@gmail.com>
2024-04-27 15:09:41 -0400

    Subiendo cosas

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ ninja -C build
 sudo ninja -C build install
 
 # Instalando p10k
-
+cd ~/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
